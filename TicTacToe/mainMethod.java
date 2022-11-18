23de35d333b14e2f3b01b0074668e4f12a282343
iyaadafifi <116849426+iyaadafifi@users.noreply.github.com>
2022-11-18 17:54:30 -0600

    Create mainMethod.java

---
+++ TicTacToe/mainMethod.java
@@ -0,0 +1,16 @@
+package test;
+
+
+	
+
+	public class mainMethod
+	{
+	    public mainMethod() {
+	        throw new Error("Unresolved compilation problem: \n\tMust declare a named package because this compilation unit is associated to the named module 'TicTacToe'\n");
+	    }
+	    
+	    public static void main(final String[] array) {
+	    	final TicTacToe game = new TicTacToe();
+	    }
+	
+}
